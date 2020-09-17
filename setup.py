3d353fc257d1f04220134995921dd6ebd00d2dbc
Boris Acalinovic <boris.acalinovic@gmail.com>
2020-09-17 12:28:18 +0200

    setup commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wikiloader',
+    version='alpha',
+    packages=['core', 'core.decorators', 'utils', 'loaders', 'parsers'],
+    url='',
+    license='GNU',
+    author='Boris Acalinovic',
+    author_email='boris.acalinovic@gmail.com',
+    description='Wikipedia data parser'
+)
